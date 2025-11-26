0987ec6179d739f78994d55367977f282a6e7d41
Jorge Kassabji <jorgekm21@gmail.com>
2025-11-26 18:14:08 -0400

    Commit 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './components/menu/menu';
+import { Home } from './components/home/home';
+import { Header } from './components/header/header';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root-new',
+  imports: [RouterOutlet, Menu, Home, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app-new.css'
 })
 export class App {
   protected readonly title = signal('angular-basico');
